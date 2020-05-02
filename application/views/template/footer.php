476d12ee026b493e8ecd73cdb30b5ff8351f921a
Aléxia Gomes <alexiagommmes@gmail.com>
2020-05-02 18:13:09 -0300

    initial commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,5 @@
+<footer>
+	<div class="container text-center">
+		<p>Espaço Aluno by Aléxia Gomes | Template by <a href="http://moozthemes.com"><span>MOOZ</span>Themes.com</a></p>
+	</div>
+</footer>
